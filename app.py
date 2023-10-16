57b08447ec27c4163fb3fb3e195da25459681840
Nam14122003 <147422976+Nam14122003@users.noreply.github.com>
2023-10-16 09:23:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template, request
-
-
-app = Flask(__name__)
+from django.shortcuts import render
+from django.http import HttpResponse
 
 # Tạo một danh sách các từ vựng và ý nghĩa tương ứng
 vocab_list = [
@@ -10,31 +8,28 @@ vocab_list = [
     {'vocab': 'bird', 'meaning': 'con chim'}
 ]
 
-# Định nghĩa route ban đầu khi trang web được mở
-@app.route('/')
-def index():
+# Định nghĩa view ban đầu khi trang web được mở
+def index(request):
     # Lấy chỉ số từ vựng đầu tiên
     index = 0
-    return render_template('flashcard.html', vocab=vocab_list[index]['vocab'], meaning=vocab_list[index]['meaning'])
+    return render(request, 'flashcard.html', {'vocab': vocab_list[index]['vocab'], 'meaning': vocab_list[index]['meaning'], 'current_index': index})
 
 # Định nghĩa chức năng next
-@app.route('/next', methods=['POST'])
-def next():
-    # Lấy chỉ số hiện tại từ form
-    current_index = int(request.form.get('current_index'))
+def next(request):
+    # Lấy chỉ số hiện tại từ request.POST
+    current_index = int(request.POST.get('current_index'))
     if current_index < len(vocab_list) - 1:
         # Chuyển tới từ vựng tiếp theo nếu có
         next_index = current_index + 1
     else:
         # Quay trở lại từ vựng đầu tiên nếu đã đến cuối danh sách từ vựng
         next_index = 0
-    return render_template('flashcard.html', vocab=vocab_list[next_index]['vocab'], meaning=vocab_list[next_index]['meaning'])
+    return render(request, 'flashcard.html', {'vocab': vocab_list[next_index]['vocab'], 'meaning': vocab_list[next_index]['meaning'], 'current_index': next_index})
 
 # Định nghĩa chức năng back to home
-@app.route('/back_to_home')
-def back_to_music():
+def back_to_home(request):
     # Mã xử lý để quay trở lại trang chủ
-    return "Quay trở lại trang chủ"
+    return HttpResponse("Quay trở lại trang chủ")
 
 if __name__ == '__main__':
-    app.run(debug= True)
+    app.run(host="0.0.0.0", debug=True)
